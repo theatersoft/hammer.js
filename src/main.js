63be6bca4419a94a4738933f62020fca709218d0
Rob Huehn <rhuehn@gmail.com>
2017-10-21 21:15:26 -0700

    replace main.js Hammer module definition code with ESM export

--- src/main.js
+++ src/main.js
@@ -123,17 +123,4 @@ assign(Hammer, {
   removeEventListeners
 });
 
-let freeGlobal = (typeof window !== 'undefined' ? window : (typeof self !== 'undefined' ? self : {})); // jshint ignore:line
-freeGlobal.Hammer = Hammer;
-
-/* jshint ignore:start */
-if (typeof define === 'function' && define.amd) {
-  define(() => {
-    return Hammer;
-  });
-} else if (typeof module !== 'undefined' && module.exports) {
-  module.exports = Hammer;
-} else {
-  window[exportName] = Hammer;
-}
-/* jshint ignore:end */
+export default Hammer
